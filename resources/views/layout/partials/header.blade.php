93dc08add2669e07831f98843fe4c14cb8ff0bf8
alaaqutfa <alaaqutfa.work@gmail.com>
2025-09-17 08:15:11 +0300

    fix user navbar error

--- resources/views/layout/partials/header.blade.php
+++ resources/views/layout/partials/header.blade.php
@@ -52,7 +52,7 @@
                     </li>
                 </ul>
             </div>
-            <button data-collapse-toggle="navbar-user" type="button"
+            {{-- <button data-collapse-toggle="navbar-user" type="button"
                 class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200"
                 aria-controls="navbar-user" aria-expanded="false">
                 <span class="sr-only">Open main menu</span>
@@ -61,7 +61,7 @@
                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                         d="M1 1h15M1 7h15M1 13h15" />
                 </svg>
-            </button>
+            </button> --}}
         </div>
         {{-- <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
             <ul
