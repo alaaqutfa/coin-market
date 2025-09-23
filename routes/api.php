c25b37e1eb22eba8e49c11c11f16bdf1a579e7c6
alaaqutfa <alaaqutfa.work@gmail.com>
2025-09-23 11:29:10 +0300

    edit routes

--- routes/api.php
+++ routes/api.php
@@ -12,13 +12,12 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::post('/employee/check-in', [AttendanceController::class, 'checkIn']);
     Route::post('/employee/check-out', [AttendanceController::class, 'checkOut']);
     Route::get('/employee/attendance', [AttendanceController::class, 'index']);
-
-    Route::apiResource('products', ProductController::class);
-    Route::get('products/barcode/{barcode}', [ProductController::class, 'findByBarcode']);
-    Route::put('products/barcode/{barcode}', [ProductController::class, 'updateByBarcode']);
 });
+Route::apiResource('products', ProductController::class);
+Route::get('products/barcode/{barcode}', [ProductController::class, 'findByBarcode']);
+Route::put('products/barcode/{barcode}', [ProductController::class, 'updateByBarcode']);
 
 // route اختبار
-Route::get('/test', function() {
+Route::get('/test', function () {
     return response()->json(['message' => 'API is working!']);
 });
--- routes/web.php
+++ routes/web.php
@@ -4,26 +4,13 @@ use App\Http\Controllers\Admin\EmployeeController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
-use Spatie\Browsershot\Browsershot;
-
-Route::get('/test-browsershot', function () {
-    Browsershot::html('<h1 style="color:red">Hello World</h1>')
-        ->setNodeBinary('C:\Program Files\nodejs\node.exe')
-        ->setNpmBinary('C:\Program Files\nodejs\npm.cmd')
-        ->setChromePath('C:\Users\AMR Service Center\.cache\puppeteer\chrome\win64-140.0.7339.82\chrome-win64\chrome.exe')
-        ->windowSize(800, 600)
-        ->save(public_path('test.png'));
-
-    return response()->download(public_path('test.png'));
-});
 
 Route::get('/', [DashboardController::class, 'dashboard'])->name('dashboard');
 Route::get('/products', [ProductController::class, 'list'])->name('products.list');
 Route::get('/filter-products', [ProductController::class, 'filter'])->name('products.filter');
 Route::post('/products/preview-images', [ProductController::class, 'previewImages'])->name('products.preview.images');
 Route::post('/products/save-images', [ProductController::class, 'saveImages'])->name('products.save.images');
-Route::post('/exportCatalog', [ProductController::class, 'exportCatalog'])->name('exportCatalog');
-Route::get('/showCatalog', [ProductController::class, 'showCatalog'])->name('showCatalog');
+Route::get('/show-catalog', [ProductController::class, 'showCatalog'])->name('showCatalog');
 
 Route::get('/employees', [EmployeeController::class, 'index'])->name('employees.index');
 Route::get('/employees/create', [EmployeeController::class, 'create'])->name('employees.create');
