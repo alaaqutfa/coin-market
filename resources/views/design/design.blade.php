5c5ad8afe8a697e7e0e9a49ed00def1ff17e5d66
alaaqutfa <alaaqutfa.work@gmail.com>
2025-09-24 11:16:47 +0300

    edit design bg

--- resources/views/design/design.blade.php
+++ resources/views/design/design.blade.php
@@ -21,7 +21,7 @@
         body {
             width: 100%;
             height: 100vh;
-            /* background: url('{{ asset('assets/img/design-bg-1.jpeg') }}') no-repeat center center; */
+            background: url('{{ asset('assets/img/design-bg-1.png') }}') no-repeat center center;
             background-size: cover;
             padding: 20px;
             overflow: hidden;
@@ -81,9 +81,9 @@
             width: 100%;
             height: 100%;
             /* @if ($products->count() > 4)
-                max-height: 400px;
-            @endif */
-            object-fit: contain;
+            max-height: 400px;
+        @endif
+        */ object-fit: contain;
         }
 
         .weight {
@@ -138,7 +138,8 @@
 
 <body>
     <div class="design-container">
-        <div class="header flex justify-between items-center p-4" style="border-bottom: 7px solid #ECC631;">
+        <div class="header flex justify-between items-center p-4"
+            style="border-bottom: 7px solid #ECC631;background:#ffffff80;">
             <div class="logo-side flex justify-center items-center flex-col gap-2">
                 <img src="{{ asset('assets/img/logo-light.png') }}" alt="Logo" />
                 <h2 class="font-bold text-nowrap" style="color:var(--secondary);font-size: 70px;">
