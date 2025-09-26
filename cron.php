4c371de375ee2e94348df625b4e90b22bd1193de
alaaqutfa <alaaqutfa.work@gmail.com>
2025-09-26 10:12:45 +0300

    update cron

--- cron.php
+++ cron.php
@@ -5,6 +5,5 @@ $laravelBasePath = __DIR__;
 echo $laravelBasePath . PHP_EOL;
 echo 'Cron Job is running' . PHP_EOL;
 
-passthru("/usr/bin/php $laravelBasePath/artisan attendance:calculate-daily-hours");
+passthru("/usr/bin/php $laravelBasePath/artisan attendance:calculate-daily-hours >> /home/u179125959/schedule.log 2>&1");
 
-?>
