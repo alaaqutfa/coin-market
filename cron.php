9619a5e96a8a8ecd8e6d990f869be91672b762e3
alaaqutfa <alaaqutfa.work@gmail.com>
2025-09-26 09:53:14 +0300

    update cron

--- cron.php
+++ cron.php
@@ -1,10 +1,11 @@
 <?php
-$laravelBasePath = __DIR__ . '/..'; // تأكد من صحة المسار حسب مكان ملف cron.php
 
-echo __DIR__;
+$laravelBasePath = __DIR__ . '/../../'; // اضبط حسب مكان مجلد Laravel بالنسبة لمجلد stock
+
+echo __DIR__ . '/../../';
 echo 'Cron Job is runing';
 
 // تنفيذ الأمر artisan المطلوب وحفظ المخرجات (اختياري)
 passthru("/usr/bin/php $laravelBasePath/artisan attendance:calculate-daily-hours >> /dev/null 2>&1");
 
-
+?>
