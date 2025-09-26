b4ce074bc0d9a4537c4ac5a17895608237df6e56
alaaqutfa <alaaqutfa.work@gmail.com>
2025-09-26 10:08:43 +0300

    update cron

--- cron.php
+++ cron.php
@@ -1,14 +1,10 @@
 <?php
 
-$laravelBasePath = __DIR__; // تأكد أن هذا هو المسار الصحيح لمجلد لارافيل (مجلد يحتوي على ملف artisan)
+$laravelBasePath = __DIR__;
 
-// طباعة المسار (فقط للتجربة، يمكنك إزالته لاحقاً)
 echo $laravelBasePath . PHP_EOL;
-
-// طباعة رسالة لتأكيد عمل الكرون (اختياري)
 echo 'Cron Job is running' . PHP_EOL;
 
-// تنفيذ أمر Artisan المطلوب لتنفيذ الحسابات المطلوبة
-passthru("/usr/bin/php $laravelBasePath/artisan attendance:calculate-daily-hours >> /dev/null 2>&1");
+passthru("/usr/bin/php $laravelBasePath/artisan attendance:calculate-daily-hours");
 
 ?>
