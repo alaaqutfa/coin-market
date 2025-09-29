9ae7e14812142335f4218fcae09e1305f721ea67
alaaqutfa <alaaqutfa.work@gmail.com>
2025-09-29 19:49:31 +0300

    update

--- resources/views/products/view.blade.php
+++ resources/views/products/view.blade.php
@@ -307,7 +307,7 @@
                         <div
                             class="no_image_div relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 rounded-full peer peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all peer-checked:bg-yellow-600">
                         </div>
-                        <span class="ms-3 text-sm font-medium text-white">منتجات لديها صور فقط</span>
+                        <span class="ms-3 text-sm font-medium text-white">منتجات ليس لديها صور فقط</span>
                     </label>
                 </form>
             </div>
