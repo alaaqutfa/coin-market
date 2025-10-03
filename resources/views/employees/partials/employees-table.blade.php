c3eca75df55194605abd78c05aa66508cf3dd899
alaaqutfa <alaaqutfa.work@gmail.com>
2025-10-03 09:51:47 +0300

    update

--- resources/views/employees/partials/employees-table.blade.php
+++ resources/views/employees/partials/employees-table.blade.php
@@ -33,7 +33,7 @@
         </td>
         <td class="px-6 py-4">
             <div class="editable-field" contenteditable="true" data-field="end_date">
-            {{ $employee->start_date->format('Y-m-d') ?? 'قيد العمل' }}
+            {{ $employee->end_date->format('Y-m-d') ?? 'قيد العمل' }}
             </div>
         </td>
         <td class="px-6 py-4">
