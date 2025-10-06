12f94996bed751bcb1075b8c2b8e7af0926ad1ae
alaaqutfa <alaaqutfa.work@gmail.com>
2025-10-06 14:30:13 +0300

    new design

--- resources/views/design/design-1.blade.php
+++ resources/views/design/design-1.blade.php
@@ -21,8 +21,9 @@
         body {
             width: 100%;
             height: 100vh;
-            background: url('{{ asset('assets/img/design-bg-3.png') }}') no-repeat center center;
+            background: url('{{ asset('assets/img/design-bg-2.png') }}') no-repeat center center;
             background-size: cover;
+            padding: 20px;
             overflow: hidden;
             display: flex;
             justify-content: center;
@@ -31,7 +32,8 @@
 
         .design-container {
             width: 100%;
-            height: 100%;
+            border: 7px solid #ECC631;
+            border-radius: 15px;
         }
 
         .header {
@@ -45,13 +47,12 @@
 
         .products {
             width: 100%;
-            min-height: calc(100vh - 30rem);
+            min-height: calc(100vh - 350px);
             display: flex;
             justify-content: center;
             align-items: center;
             flex-wrap: wrap;
-            gap: 1rem;
-            padding: 1rem;
+            gap: 3rem;
         }
 
         .product {
@@ -59,26 +60,26 @@
             display: flex;
             justify-content: center;
             align-items: end;
-            flex-direction: column;
             /* gap: 1rem; */
         }
 
         .image-shape {
             position: relative;
-            width: 100%;
+            width: 60%;
+
             @if (count($products) > 4)
-                height: 300px;
+                height: 500px;
             @else
-                height: 400px;
+                height: 600px;
             @endif
-            padding: 1rem;
+            padding: 2rem;
             display: flex;
             justify-content: center;
             align-items: center;
             flex-direction: column;
             gap: 1rem;
-            /* border: 5px solid var(--primary); */
-            border-radius: 20px 20px 0px 0px;
+            border: 5px solid var(--primary);
+            border-radius: 20px 20px 0px 20px;
         }
 
         .image-shape .product-image {
@@ -90,43 +91,51 @@
         */ object-fit: contain;
         }
 
-        .price-weight-shape {
-            display: flex;
-            justify-content: space-between;
-            align-items: center;
-            position: absolute;
-            bottom: 0%;
-            left: 0%;
-            width: 100%;
-            height: 15%;
-            padding: 0.5rem;
-            background-color: var(--primary);
-        }
-
         .weight {
-            color: var(--secondary);
+            position: absolute;
+            top: 0%;
+            left: 0%;
+            display: flex;
+            justify-content: flex-end;
+            align-items: center;
+            height: 15%;
+            padding: 1rem;
+            color: white;
+            background-color: red;
+            border-radius: 15px 0px 15px;
             font-size: 3rem;
             font-weight: 800;
             line-height: 2rem;
         }
 
         .price-shape {
-            width: 100%;
+            width: 40%;
+            background: #f8f8f8;
+            border-top: 5px solid var(--primary);
+            border-right: 5px solid var(--primary);
             box-shadow: 20px 10px 10px #22222280;
         }
 
         .name {
             width: 100%;
             padding: 1rem;
-            color: var(--primary);
-            font-size: 2.25rem;
+            color: var(--secondary);
+            font-size: 3rem;
             font-weight: 800;
-            text-align: center;
+            line-height: 4rem;
+            text-align: start;
         }
 
         .price {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            width: 100%;
+            height: 25%;
+            padding: 1rem;
             color: var(--secondary);
-            font-size: 3rem;
+            background-color: var(--primary);
+            font-size: 4rem;
             font-weight: 800;
         }
     </style>
@@ -134,34 +143,50 @@
 
 <body>
     <div class="design-container">
-        <div class="top-products" style="height: 18.5rem;"></div>
+        <div class="header flex justify-between items-center p-4" style="border-bottom: 7px solid #ECC631;">
+            <div class="logo-side flex justify-center items-center flex-col gap-2">
+                <img src="{{ asset('assets/img/logo-light.png') }}" alt="Logo" />
+                <h2 class="font-bold text-nowrap" style="color:var(--secondary);font-size: 70px;">
+                    Coin <span style="color: #ecc631;">Market</span>
+                </h2>
+            </div>
+            <div class="contact flex justify-center items-center gap-4">
+                <div class="whatsapp flex justify-center items-center gap-4">
+                    <i class="fa-brands fa-whatsapp" style="font-size:75px;color: #ECC631;"></i>
+                    <span class="font-bold" style="color:var(--secondary);font-size: 70px;">71 34 97 93</span>
+                </div>
+                <div class="phone flex justify-center items-center gap-4">
+                    <i class="fa-solid fa-phone" style="font-size:75px;color: #ECC631;"></i>
+                    <span class="font-bold" style="color:var(--secondary);font-size: 70px;">09 21 26 72</span>
+                </div>
+            </div>
+            <div class="shape-side">
+                <img src="{{ asset('assets/img/design-shape-1.png') }}" alt="">
+            </div>
+        </div>
         <div class="products">
             @foreach ($products as $product)
                 <div class="product">
                     <div class="image-shape">
                         <img class="product-image" src="{{ asset('storage/' . $product->image_path) }}"
                             alt="product image" />
-                            <div class="price-weight-shape">
-                                <span class="price">
-                                    {{ $product->price }}$
-                                </span>
-                                @if ($product->weight != 0)
-                                    <span class="weight">
-                                        {{ $product->weight }}
-                                    </span>
-                                @endif
-                            </div>
+                        @if ($product->weight != 0)
+                            <span class="weight">
+                                {{ $product->weight }}
+                            </span>
+                        @endif
                     </div>
                     <div class="price-shape">
                         <h5 class="name">
                             {{ $product->name }}
                         </h5>
-
+                        <span class="price">
+                            {{ $product->price }}$
+                        </span>
                     </div>
                 </div>
             @endforeach
         </div>
-        <div class="contct-products" style="height: 10rem;"></div>
     </div>
 
     <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>\
