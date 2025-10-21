e2f076b9ded74ceec1de7a239a570f8cc94d719a
alaaqutfa <alaaqutfa.work@gmail.com>
2025-10-21 13:53:39 +0300

    edit

--- resources/views/customer/product.blade.php
+++ resources/views/customer/product.blade.php
@@ -55,7 +55,7 @@
                         </div>
                         <div class="flex items-center justify-between">
                             <span class="text-3xl font-bold text-gray-900">
-                                {{ $product['symbol'] ?? '$' }}{{ $product['price'] }}
+                                {{ $product['price'] }} {{ $product['symbol'] ?? '$' }}
                             </span>
                             <a href="#"
                                 class="text-white bg-yellow-400 hover:bg-yellow-500 focus:ring-4 focus:outline-none focus:ring-yellow-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
