3c03cde76a5397cda2c2966c87e1440e01899731
alaaqutfa <alaaqutfa.work@gmail.com>
2025-10-21 12:23:19 +0300

    add migrations

--- database/migrations/2025_10_21_121918_create_brands_table.php
+++ database/migrations/2025_10_21_121918_create_brands_table.php
@@ -11,8 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('employees', function (Blueprint $table) {
-            $table->date('end_date')->nullable()->after('start_date');
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('logo')->nullable();
+            $table->timestamps();
         });
     }
 
@@ -21,8 +24,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('employees', function (Blueprint $table) {
-            $table->dropColumn('end_date');
-        });
+        Schema::dropIfExists('brands');
     }
 };
