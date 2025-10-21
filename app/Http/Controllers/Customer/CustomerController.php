c9acd04772980d2c86e770889a241421a8490762
alaaqutfa <alaaqutfa.work@gmail.com>
2025-10-21 12:13:41 +0300

    edit

--- app/Http/Controllers/Customer/CustomerController.php
+++ app/Http/Controllers/Customer/CustomerController.php
@@ -9,7 +9,7 @@ use App\Models\Product;
 class CustomerController extends Controller
 {
     public function home(){
-        $products = Product::whereNotNull('image_path')->latest()->paginate(20);
+        $products = Product::whereNotNull('image_path')->latest()->paginate(27);
         return view('customer.product',compact('products'));
     }
 }
