f886cd4fd6e6d09f618c45aaf12bfe56b0092809
alaaqutfa <alaaqutfa.work@gmail.com>
2026-01-06 16:48:26 +0200

    test design

--- resources/views/meat-inventory/daily-sales/report.blade.php
+++ resources/views/meat-inventory/daily-sales/report.blade.php
@@ -212,13 +212,13 @@
                                         {{ $sale->sale_date->translatedFormat('l') }}
                                     </div>
                                 </td>
-                                <td class="px-6 py-4">
+                                <td class="flex justify-center items-center px-6 py-4">
                                     @if ($sale->image)
                                         <img src="{{ asset('storage/app/public/' . $sale->image) }}" alt="صورة المنتج"
-                                            class="w-12 h-12 object-cover rounded-md border border-gray-200">
+                                            class="w-12 hover:w-80 h-12 hover:h-80 object-cover rounded-md border border-gray-200">
                                     @else
                                         <div
-                                            class="w-12 h-12 flex items-center justify-center bg-gray-100 dark:bg-gray-700 rounded-md border border-gray-200">
+                                            class="w-12 hover:w-80 h-12 hover:h-80 flex items-center justify-center bg-gray-100 dark:bg-gray-700 rounded-md border border-gray-200">
                                             <i class="fas fa-image text-gray-400"></i>
                                         </div>
                                     @endif
